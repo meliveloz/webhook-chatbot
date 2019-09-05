e4da0b588eeef6730986bed1f0124e973d9d9199
Melissa Veloz <mveloz@entel.cl>
2019-09-05 11:29:12 -0400

    last

--- index.js
+++ index.js
@@ -4,12 +4,10 @@
 const
   express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()), // creates express http server
-  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
-  request = require('request');
+  app = express().use(bodyParser.json()); // creates express http server
+
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
  
@@ -21,25 +19,11 @@ app.post('/webhook', (req, res) => {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      
-      // Gets the body of the webhook event
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
-
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);        
-      } else if (webhook_event.postback) {
-      handlePostback(sender_psid, webhook_event.postback);
-    }
-  
-  });
+    });
 
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
@@ -49,12 +33,11 @@ app.post('/webhook', (req, res) => {
   }
 
 });
-
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
+  let VERIFY_TOKEN = "holahola"
     
   // Parse the query params
   let mode = req.query['hub.mode'];
