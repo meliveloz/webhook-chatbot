9c0147a07e2a675906bd71c03bf2427b9b31432f
Melissa Veloz <mveloz@entel.cl>
2019-09-05 11:43:37 -0400

    hola

--- index.js
+++ index.js
@@ -1,62 +1,48 @@
-'use strict';
+// Importar las dependencias para configurar el servidor
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
 
-// Imports dependencies and set up http server
-const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// configurar el puerto y el mensaje en caso de exito
+app.listen((process.env.PORT || 5000), () => console.log('El servidor webhook esta escchando!'));
 
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
+// Ruta de la pagina index
+app.get("/", function (req, res) {
+    res.send("Se ha desplegado de manera exitosa el CMaquera ChatBot :D!!!");
 });
-// Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
 
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "holahola"
-    
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
+// Facebook Webhook
+
+// Usados para la verificacion
+app.get("/webhook", function (req, res) {
+    // Verificar la coincidendia del token
+    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+        // Mensaje de exito y envio del token requerido
+        console.log("webhook verificado!");
+        res.status(200).send(req.query["hub.challenge"]);
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+        // Mensaje de fallo
+        console.error("La verificacion ha fallado, porque los tokens no coinciden");
+        res.sendStatus(403);
     }
-  }
+});
+
+// Todos eventos de mesenger sera apturados por esta ruta
+app.post("/webhook", function (req, res) {
+    // Verificar si el vento proviene del pagina asociada
+    if (req.body.object == "page") {
+        // Si existe multiples entradas entraas
+        req.body.entry.forEach(function(entry) {
+            // Iterara todos lo eventos capturados
+            entry.messaging.forEach(function(event) {
+                if (event.message) {
+                    process_event(event);
+                }
+            });
+        });
+        res.sendStatus(200);
+    }
 });
