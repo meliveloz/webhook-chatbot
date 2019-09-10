cdf5b4131019ae7fb8866d7e4b8db0cc5afc9326
Melissa Veloz <mveloz@entel.cl>
2019-09-10 15:49:41 -0300

    fix sender action

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ function handlePostback(event) {
     return new Promise((resolve, reject) => {
       request({
         url: 'https://graph.facebook.com/v3.1/me/messages',
-        qs: {access_token: process.env.FB_ACCESS_TOKEN},
+        qs: {access_token: process.env.process.env.PAGE_ACCESS_TOKEN},
         method: 'POST',
         json: {
           recipient: {id: data},
@@ -159,9 +159,9 @@ function handlePostback(event) {
         }
       }, (error, response) => {
         if (error) {
-          reject('ERROR_FACEBOOK_SENDING_ACTION=' + error);
+          reject('ERROR_FACEBOOK_SENDING_ACTION=');
         } else if (response.body.error) {
-          reject('ERROR_FACEBOOK_SENDING_ACTION=' + response.body.error);
+          reject('ERROR_FACEBOOK_SENDING_ACTION=');
         }
   
         resolve(data);
