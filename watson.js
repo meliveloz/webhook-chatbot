ae8f0cde809f508fff14e64250d01be0f573595f
Melissa Veloz <mveloz@entel.cl>
2019-10-01 17:44:38 -0300

    fixing watson

--- watson.js
+++ watson.js
@@ -1,6 +1,8 @@
 const AssistantV1 = require('ibm-watson/assistant/v1');
 
 function watsonIntegration(event){
+  console.log('este es el event.message.text');
+  console.log (event.message.text);
     const service = new AssistantV1({
         version: '2019-02-28',
         iam_apikey: 'ZWrKTYlOCWc27ZDnjHir2n-LSDcWwU8AQKIT4Wk7KydH',
@@ -11,7 +13,6 @@ function watsonIntegration(event){
         workspace_id: '9d0ddbc8-379f-4fee-bd8f-318181038722',
         input: {'text': event.message.text}
         })
-        console.log('este es el event.message.text');
-        console.log (event.message.text);
+       
 }
 module.exports = {watsonIntegration};
