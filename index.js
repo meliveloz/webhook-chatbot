b67ef954dede677f746fa4c78985c8be45e6fa19
Melissa Veloz <mveloz@entel.cl>
2019-10-04 12:43:29 -0300

    test

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post("/webhook", function (req, res) {
                                     console.log(data.response_type);
                                     if(data.response_type === 'option'){
                                       
-                                        console.log(data.options);
+                                        console.log(data.options[0]);
                                     }
                                    else  if (data.response_type === 'text'){
                                         console.log(JSON.stringify(res, null, 2));
